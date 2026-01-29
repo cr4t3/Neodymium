f59a063070b875ac14a44d1e687f2ff0a595aacb
crate <67573485+cr4t3@users.noreply.github.com>
2026-01-29 01:48:04 -0300

    Optimized CPU usage

--- src/modules/virtualizer.cpp
+++ src/modules/virtualizer.cpp
@@ -1,5 +1,7 @@
 #include "virtualizer.h"
 #include "arch.h"
+#include <thread>
+#include <chrono>
 
 Virtualizer::Virtualizer(BaseArch* arch)
 : arch(arch)
@@ -14,5 +16,6 @@ int Virtualizer::run() {
         {
             return res;
         }
+        std::this_thread::sleep_for(std::chrono::milliseconds(1)); // Makes the CPU usage go from 100% to around 3% (according with neo8 testing)
     }
 }
